0e828f17773104e340d4dea96a609bd5d05c8b6d
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2024-06-11 02:28:13 +0000

    Built site for gh-pages

--- assets/map.js
+++ assets/map.js
@@ -2,11 +2,14 @@
 // import * as Cesium from "https://cesium.com/downloads/cesiumjs/releases/1.87.1/Build/Cesium/Cesium.js";
 
 async function makeWidget(elementId) {
-    // Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4NjQ5YjhjOS01Y2UyLTRhYWEtOGEzZC1hZDZmZWI0NGQ3MTQiLCJpZCI6MjIxMjE4LCJpYXQiOjE3MTgwMDk5MDB9._pdWlrUxdAtIiuq3PXK8HrRNxcoJa6bOjwWE8cQ3J3Y';
+    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4NjQ5YjhjOS01Y2UyLTRhYWEtOGEzZC1hZDZmZWI0NGQ3MTQiLCJpZCI6MjIxMjE4LCJpYXQiOjE3MTgwMDk5MDB9._pdWlrUxdAtIiuq3PXK8HrRNxcoJa6bOjwWE8cQ3J3Y';
     const viewer = new Cesium.Viewer(elementId, {
         timeline: false,
         animation: false,
         baseLayerPicker: false,
+        geocoder: false,
+        homeButton: false,
+        sceneModePicker: false,
     });
     const scene = viewer.scene;
     scene.globe.depthTestAgainstTerrain = true;
@@ -26,25 +29,27 @@ async function makeWidget(elementId) {
         scene.globe.show = false;
         worldTileset.show = true;
     } catch (error) {
-        console.log(`Error loading Photorealistic 3D Tiles tileset.
-      ${error}`);
+        console.log(`Error loading Photorealistic 3D Tiles tileset. ${error}`);
     }
     // TODO: use open-source tile providers to get rid of warnings.
     // https://gist.github.com/banesullivan/e3cc15a3e2e865d5ab8bae6719733752#file-cesium-viewer-no-token-html
 
-    viewer.dataSources.add(
-        Cesium.GeoJsonDataSource.load(
-            // consider using simplestyle-spec to style the geojson
-            // https://github.com/mapbox/simplestyle-spec/tree/master/1.1.0
-            
-            // Needs to use the raw.githubusercontent.com link to avoid CORS issues per
-            // https://stackoverflow.com/a/71359956/5156887
-            "https://raw.githubusercontent.com/NickCrews/turnagain-pass-guidebook/main/assets/objects.geojson",
-            //   "../../assets/objects.geojson",
-          {
+    const dataSource = Cesium.GeoJsonDataSource.load(
+        // consider using simplestyle-spec to style the geojson
+        // https://github.com/mapbox/simplestyle-spec/tree/master/1.1.0
+
+        // Needs to use the raw.githubusercontent.com link to avoid CORS issues per
+        // https://stackoverflow.com/a/71359956/5156887
+        "https://raw.githubusercontent.com/NickCrews/turnagain-pass-guidebook/main/assets/objects.geojson",
+        //   "../../assets/objects.geojson",
+        {
             clampToGround: true,
             credit: "",
-          }
-        )
-      );
+        }
+    )
+    viewer.dataSources.add(dataSource);
+    viewer.zoomTo(
+        dataSource, new Cesium.HeadingPitchRange(Cesium.Math.toRadians(135), Cesium.Math.toRadians(-50), 2000)
+    );
+
 }
